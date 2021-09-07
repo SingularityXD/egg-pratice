424fb0a185f1c1be05ae84bd0ef3f31104bccd55
tony.luo <tony.luo@huolala.cn>
2021-09-07 17:40:19 +0800

    feat: ch-06

--- config/config.default.js
+++ config/config.default.js
@@ -1,25 +1,6 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
-const mysql = {
-  // 单数据库信息配置
-  client: {
-    // host
-    host: 'localhost',
-    // 端口号
-    port: '3306',
-    // 用户名
-    user: 'root',
-    // 密码
-    password: '151659225', // 初始化密码，没设置的可以不写
-    // 数据库名
-    database: 'juejue_cost', // 我们新建的数据库名称
-  },
-  // 是否加载到 app 上，默认开启
-  app: true,
-  // 是否加载到 agent 上，默认关闭
-  agent: false,
-};
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -39,6 +20,28 @@ module.exports = appInfo => {
     },
     domainWhiteList: [ '*' ], // 配置白名单
   };
+  config.jwt = {
+    secret: 'Nick',
+  };
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '151659225', // 初始化密码，没设置的可以不写
+      // 数据库名
+      database: 'juejue_cost', // 我们新建的数据库名称
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
 
   // add your middleware config here
   config.middleware = [];
@@ -51,6 +54,5 @@ module.exports = appInfo => {
   return {
     ...config,
     ...userConfig,
-    mysql,
   };
 };
